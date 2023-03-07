812c5d0e144d1171cf59d7b4131f188d489a614e
TengZhang <366205968@qq.com>
2023-03-07 13:21:02 -0500

    Using AWS Secret Manager for the database credentials
    Using AWS Secret Manager for the database credentials

--- src/main/java/com/example/authenticationservice/config/HibernateProperty.java
+++ src/main/java/com/example/authenticationservice/config/HibernateProperty.java
@@ -1,30 +1,32 @@
 package com.example.authenticationservice.config;
 
+import com.example.authenticationservice.domain.AwsSecrets;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import lombok.*;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @Builder
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class HibernateProperty {
 
-    @Value("${database.hibernate.url}")
     private String url;
 
     @Value("${database.hibernate.driver}")
     private String driver;
 
-    @Value("${database.hibernate.username}")
     private String username;
 
-    @Value("${database.hibernate.password}")
     private String password;
 
     @Value("${database.hibernate.dialect}")
@@ -32,5 +34,45 @@ public class HibernateProperty {
 
     @Value("${database.hibernate.showsql}")
     private String showsql;
+
+    public static AwsSecrets getSecret() {
+
+        String secretName = "Mysql";
+        Region region = Region.of("us-east-2");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            // For a list of exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        String secret = getSecretValueResponse.secretString();
+
+
+        Gson gson = new Gson();
+        return gson.fromJson(secret, AwsSecrets.class);
+
+    }
+
+    public HibernateProperty(){
+        AwsSecrets awsSecrets = getSecret();
+        this.username = awsSecrets.getUsername();
+        this.password = awsSecrets.getPassword();
+        // jdbc:mysql://team1-hrwebsite-database.cobx6uz1hjmg.us-east-2.rds.amazonaws.com:3306/ApplicationService?useSSL=false
+        this.url = "jdbc:" + awsSecrets.getEngine() + "://" + awsSecrets.getHost() + ":" + awsSecrets.getPort() + "/HousingService?useSSL=false";
+    }
 }
 
