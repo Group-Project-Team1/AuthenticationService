6d40a9e025446ec84546481cfd394c1916ef16f2
Bao Nguyen <btn6364@rit.edu>
2023-03-07 13:40:43 -0500

    Add JWTFilter to AuthenticationService

--- src/main/java/com/example/authenticationservice/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.authenticationservice.config;
 
+import com.example.authenticationservice.security.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //WebSecurityConfigurerAdapter needs to extended to override some of its methods
 @Configuration
@@ -19,6 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     public void setUserDetailsService(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    public void setJwtFilter(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
     }
 
     // authentication provider uses the userDetailsService by calling the loadUserByUsername()
@@ -38,9 +46,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     //This method is used to configure the security of the application
     //Since we are attaching jwt to a request header manually, we don't need to worry about csrf
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+            .csrf().disable()
+            .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .authorizeRequests()
+            .antMatchers("api/hr/*").hasAuthority("hr")
+            .antMatchers("api/employee/*").hasAuthority("employee")
+            .anyRequest()
+            .authenticated();
     }
 }
 
