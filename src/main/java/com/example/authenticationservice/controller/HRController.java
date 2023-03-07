13b2927adf9f1431755d028155d1a1ab07cd0ab2
TengZhang <366205968@qq.com>
2023-03-07 16:10:25 -0500

    Add the feature of sending registration token by email
    Add the feature of sending registration token by email

--- src/main/java/com/example/authenticationservice/controller/HRController.java
+++ src/main/java/com/example/authenticationservice/controller/HRController.java
@@ -6,6 +6,8 @@ import com.example.authenticationservice.domain.response.RegistrationTokenRespon
 import com.example.authenticationservice.exception.InvalidAuthorityException;
 import com.example.authenticationservice.service.RegistrationTokenService;
 import com.example.authenticationservice.service.UserService;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("api/hr/")
 public class HRController {
     private RegistrationTokenService registrationTokenService;
     private UserService userService;
-
+    private RabbitTemplate rabbitTemplate;
     @Autowired
-    public HRController(RegistrationTokenService registrationTokenService, UserService userService) {
+    public HRController(RegistrationTokenService registrationTokenService, UserService userService, RabbitTemplate rabbitTemplate) {
         this.registrationTokenService = registrationTokenService;
         this.userService = userService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @GetMapping("createToken")
@@ -39,7 +44,13 @@ public class HRController {
             throw new InvalidAuthorityException(username);
         }
         RegistrationToken token = registrationTokenService.createToken(email, currentUser);
-        // TODO: send the token to the given email.
+
+        String messageBody = "Link to registration page: localhost:9999/api/employee/register\n Token: " + token.getToken();
+        Message message = new Message(messageBody.getBytes(StandardCharsets.UTF_8));
+        message.getMessageProperties().getHeaders().put("subject", "This is your token for registration");
+        message.getMessageProperties().getHeaders().put("recipient", email);
+        rabbitTemplate.convertAndSend("email.direct", "emailKey", message);
+
         return RegistrationTokenResponse.builder()
                 .message("Created a new token...")
                 .registrationToken(token)
